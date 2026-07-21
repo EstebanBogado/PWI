70eb5be9640602effacae9484663c05c950d8fea
estebanito0602 <estebanito0602@gmail.com>
2026-07-21 02:44:47 -0300

    formulario funcionando

--- PRÁCTICA FINAL 2026/formulario/script.js
+++ PRÁCTICA FINAL 2026/formulario/script.js
@@ -1,45 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("formulario");
+    // document.getElementById("user").value = "";
+    // document.getElementById("pass").value = "";
+    // document.getElementById("confirmPass").value = "";
 
     form.addEventListener("submit", function (enviar) {
-
         enviar.preventDefault();
-
         const usuario = document.getElementById("user").value;
         const password = document.getElementById("pass").value;
         const confirmPassword = document.getElementById("confirmPass").value;
         const registro = document.getElementById("registrar");
-        const mensajeDeError = document.getElementById("mensajeDeError");
-
+        const errorUser = document.getElementById("mensajeDeError");
+        const errorPass = document.getElementById("errorPass");
+        const errorConfirmPass = document.getElementById("errorConfirmPass");
 
         // Validaciones
         if (usuario.length < 8 || usuario.length > 10) {
             //alert("El usuario debe tener entre 8 y 10 caracteres");
-            mensajeDeError.textContent = "El usuario debe tener entre 8 y 10 caracteres";
-            mensajeDeError.hidden = false;
+            errorUser.textContent = "El usuario debe tener entre 8 y 10 caracteres";
+            errorUser.hidden = false;
             return;
         }
         const usuarioRegex = /^[A-Za-z]{8,10}$/;
         if (!usuarioRegex.test(usuario)) {
             //alert("El usuario debe contener solo letras");
-            mensajeDeError.textContent = "El usuario debe contener solo letras";
-            mensajeDeError.hidden = false;
+            errorPass.textContent = "El usuario debe contener solo letras";
+            errorPass.hidden = false;
             //alert("El usuario debe contener solo letras");
             return;
         }
+        errorUser.hidden = true;
+
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{10,}$/;
         if (!passwordRegex.test(password)) {
             //alert("La contraseña debe tener al menos 10 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial");
-            mensajeDeError.textContent = "La contraseña debe tener al menos 10 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial";
-            mensajeDeError.hidden = false;
+            errorPass.textContent = "La contraseña debe tener al menos 10 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial";
+            errorPass.hidden = false;
             return;
         }
+        errorPass.hidden = true;
         if (password !== confirmPassword) {
             //alert("Las contraseñas no coinciden");
-            mensajeDeError.textContent = "Las contraseñas no coinciden";
-            mensajeDeError.hidden = false;
+            errorConfirmPass.textContent = "Las contraseñas no coinciden";
+            errorConfirmPass.hidden = false;
             return;
         }
+        errorConfirmPass.hidden = true;
         registro.disabled = false;
         window.location.href = "index.html";
 
